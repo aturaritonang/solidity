bfdfa6be35f96931298f98df0f6442610f794cb4
"Atur Aritonang" <"mangatur.aritonang@gmail.com">
2022-05-22 08:51:56 +0700

    add store

--- contracts/RentMyCar.sol
+++ contracts/RentMyCar.sol
@@ -1,23 +1,32 @@
 //SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.7.0;
+// import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract RentCar {
+
     address payable public owner;
+
+    bool public open;
+
+    struct Car {
+        string name;
+        string price;
+        bool available;
+        address customer; 
+    }
+
+    enum availability { Yes, No, All }
 
     Car[] public cars;
 
     constructor(){
-        cars.push(Car({ name: "Car 1", price : 1, available : true }));
-        cars.push(Car({ name: "Car 2", price : 2, available : true }));
-        cars.push(Car({ name: "Car 3", price : 3, available : true }));
-        cars.push(Car({ name: "Car 4", price : 2, available : true }));
-    }
-
-    struct Car {
-        string name;
-        uint price;
-        bool available;
+        owner = msg.sender;
+        open = true;
+        cars.push(Car({ name: "Car 1", price : "1000000", available : true, customer: address(0) }));
+        cars.push(Car({ name: "Car 2", price : "2000000", available : false, customer: address(0) }));
+        cars.push(Car({ name: "Car 3", price : "1500000", available : true, customer: address(0) }));
+        cars.push(Car({ name: "Car 4", price : "2000000", available : true, customer: address(0) }));
     }
 
     modifier isOwner {
@@ -30,17 +39,47 @@ contract RentCar {
         _;
     }
 
-    function addCar(string memory _name, uint _price, bool _available) external isOwner {
+    modifier isOpenStore {
+        require(open == true, "Sorry, store is closed!");
+        _;
+    }
+
+    function OpenStore() public isOwner returns(string memory) {
+        open = true;
+        return "Store is open";
+    }
+
+    function CloseStore() public isOwner returns(string memory){
+        open = false;
+        return "Store is close";
+    }
+
+    function addCar(string memory _name, string memory _price, bool _available) external isOwner {
         Car memory newCar = Car({
             name : _name,
             price : _price,
-            available : _available
+            available : _available,
+            customer: address(0)
         });
         cars.push(newCar);
     }
 
-    function checkAllCar() external isOwner {
-        
+    function checkAllCar() public view isOpenStore returns(string memory){
+        string memory names; 
+        for(uint i = 0; i < cars.length; i++){
+            string memory strAvailable;
+            if( cars[i].available ) {
+                strAvailable = 'Yes';
+            } else {
+                strAvailable = 'No';
+            }
+            string memory name = string(abi.encodePacked('Name: ', cars[i].name, ', Price: ', cars[i].price, ' Wei, Available: ', strAvailable));
+            if(i < cars.length - 1){
+                name = string(abi.encodePacked(name, ', '));
+            }
+            names = string(abi.encodePacked(names, name));
+        }
+        return names;
     }
 
     function checkAvailable(string memory _carName) external isNotOwner {
